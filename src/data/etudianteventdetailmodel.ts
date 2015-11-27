5f6592f7dc4caefdef5c14e5f672cb970792e2ed
Boubacar Diarra <boubadiarra@gmail.com>
2015-11-27 23:33:00 +0100

    work on models

--- src/data/etudianteventdetailmodel.ts
+++ src/data/etudianteventdetailmodel.ts
@@ -1,75 +1,52 @@
 //etudianteventdetailmodel.ts
 //
 import {UserInfo} from './userinfo';
-import {BaseDetailModel} from './basedetailmodel';
+import {EventDetailModel} from './eventdetailmodel';
 import {IGroupeEvent, IEtudiantEvent, IEtudiantAffectation} from 'infodata';
 import {EVT_NOTE} from './infoconstants';
 //
-export class EtudEventDetailModel extends BaseDetailModel<IEtudiantEvent> {
+export class EtudiantEventDetailModel extends EventDetailModel<IEtudiantEvent> {
 	//
-    private _canEdit: boolean = false;
-    //
     constructor(userinfo: UserInfo) {
         super(userinfo);
         this.title = "Détails Evènement";
     }
-	public get canEdit(): boolean {
-		if ((this._canEdit === undefined) || (this._canEdit === null)) {
-			this._canEdit = false;
-		}
-		return this._canEdit;
-	}
-	public get isReadOnly(): boolean {
-		return (!this.canEdit);
-	}
-    public activate(params?: any, config?: any, instruction?: any): any {
-		let id: string = null;
-		if (params.id !== undefined) {
-			id = params.id;
-		}
-		let p = this.event;
-		if (p !== null) {
-			if (p.url !== null) {
-				this.revokeUrl(p.url);
-			}
-		}
-		this._canEdit = false;
-		this.event = null;
-        return this.initialize_event(id).then((b) => {
-			let pp: IEtudiantEvent = this.event;
+	protected initialize_item(evtid: string): Promise<boolean> {
+		return super.initialize_item(evtid).then((b) => {
+			let pp: IEtudiantEvent = this.currentItem;
 			if (pp === null) {
-				this.event = this.itemFactory.create_etudiantevent();
+				this.currentItem = this.itemFactory.create_etudiantevent();
 				this.title = "Détails Evènement";
 			} else {
-				this.title = this.event.fullname;
+				this.title = this.currentItem.fullname;
 			}
-			let gvtid = this.event.groupeeventid;
+			let gvtid = this.currentItem.groupeeventid;
 			return this.dataService.find_item_by_id(gvtid);
 		}).then((paff: IGroupeEvent) => {
 			if ((paff !== undefined) && (paff !== null)) {
 				if (this.is_prof) {
 					let cont = this.person.eventids;
-					this._canEdit = this.contains_array_id(cont, paff.id);
+					this.canEdit = this.contains_array_id(cont, paff.id);
 				}
 			}// paff
 			return true;
 		});
-    }// activate
+	}
 	public get groupeEventName(): string {
-		return (this.event !== null) ? this.event.groupeEventName : null;
+		return (this.currentItem !== null) ? this.currentItem.groupeEventName : null;
 	}
 	public get groupeeventid():string {
-		return (this.event !== null) ? this.event.groupeeventid : null;
+		return (this.currentItem !== null) ? this.currentItem.groupeeventid : null;
 	}
 	public get etudiantid():string {
-		return (this.event !== null) ? this.event.etudiantid : null;
+		return (this.currentItem !== null) ? this.currentItem.etudiantid : null;
 	}
     public get note(): string {
-		return (this.event !== null) ? this.number_to_string(this.event.note) : null;
+		return (this.currentItem !== null) ? this.number_to_string(this.currentItem.note) : null;
     }
     public set note(s: string) {
-        if (this.event !== null) {
-			this.event.note = this.string_to_number(s);
+        if (this.currentItem !== null) {
+			this.currentItem.note = this.string_to_number(s);
 		}
     }
 
